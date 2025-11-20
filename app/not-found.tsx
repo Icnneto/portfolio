b62626f597dd2f2737762e50032dfd777caf93f3
Israel Nunes <israelnetonunes@gmail.com>
2025-11-20 19:58:54 -0300

    feat: implement not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="section">
+      <h2 className="section-heading">404</h2>
+      <p className="intro-text">
+        Oopss... Something went wrong!
+      </p>
+    </div>
+  )
+}
